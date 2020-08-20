6c44c85ac0539a755234347058baff7d8307d468
lancelot_lewis <LancelotLewis@users.noreply.github.com>
2020-08-20 09:23:43 +0800

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,12 +1,8 @@
 function fish_right_prompt
-  set -l color_normal (set_color normal)
-  set -l color_error (set_color $fish_color_error)
-  set -l color "$color_normal"
-  
+  set_color 555
   set -l duration "$CMD_DURATION$cmd_duration"
-  echo -sn "$color"(echo "$duration" | humanize_duration)"$color_normal "
+  echo -sn "(echo "$duration" | humanize_duration) "
   
-  set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
   date "+%H:%M:%S"
   set_color normal
 end
