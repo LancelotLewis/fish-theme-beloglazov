8707dd086626f51c025017ac5d8e13d3c881f8ef
lancelot_lewis <LancelotLewis@users.noreply.github.com>
2020-12-16 16:47:39 +0800

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,7 +1,7 @@
 function fish_right_prompt
   set_color 555
   set -l duration "$CMD_DURATION$cmd_duration"
-  echo -sn (echo "$duration" | humanize_duration)" "
+  echo -sn (echo "$duration" | humantime)" "
   
   echo (date "+%H:%M:%S")
 end
